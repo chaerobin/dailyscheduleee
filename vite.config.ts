ac5c03b0cf442aa4ea126734cf8427a9ddc4f0d7
chaerobin <chae.robin@gmail.com>
2026-05-07 00:36:06 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/dailyscheduleee/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, './src'),
     },
-    dedupe: ["react", "react-dom"],
   },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-});
+})
